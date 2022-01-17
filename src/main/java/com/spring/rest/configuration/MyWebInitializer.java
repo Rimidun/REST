8b614151c6d86ff9f6b35546623b1697954f58a8
Andrey <x137@yandex.ru>
2022-01-17 23:39:52 +0300

    refactoring file

--- src/main/java/com/spring/rest/configuration/MyWebInitializer.java
+++ src/main/java/com/spring/rest/configuration/MyWebInitializer.java
@@ -3,6 +3,7 @@ package com.spring.rest.configuration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
